c6cc01950a8e873d4825c8c75e0ae56599222259
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-10-15 18:11:30 -0400

    whoopsie

--- bot.py
+++ bot.py
@@ -8,6 +8,4 @@ bot = commands.Bot(command_prefix='*')
 bot.add_cog(Main(bot))
 
 
-# bot.run(os.environ['TOKEN'])
-# bot.run(os.environ['TOKEN'])
-bot.run('ODk4Njg5OTE1NjM2ODI2MTkz.YWn4NA._cpZyuJzlCsXeTIhJ6OeEc0nxzg')
+bot.run(os.environ['TOKEN'])
