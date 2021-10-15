29f8da3761d7b26f6017072e7f83f03b95342d8d
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-10-15 18:08:02 -0400

    finished?

--- components/main.py
+++ components/main.py
@@ -1,5 +1,7 @@
+import asyncio
 import discord.ext.commands as commands
 import aiohttp
+
 
 class Main(commands.Cog):
     def __init__(self, bot) -> None:
@@ -7,11 +9,15 @@ class Main(commands.Cog):
         self.session = aiohttp.ClientSession
 
     @commands.command(name='events')
-    async def show_events(self, ctx):
+    async def show_events(self, ctx, events=5):
         async with self.session() as session:
             resp = await session.get('https://thrillshare-cmsv2.services.thrillshare.com/api/v4/o/5080/cms/events?locale=en')
             json = await resp.json()
-            event = json['events'][0]
-            await ctx.send(f'{event["title"]} - {event["cms_formatted_date"]}')
+            
+            for i in range(events):
+                async with ctx.typing():
+                    event = json[i]
+                    await ctx.send(f'{event["title"]} - {event["cms_formatted_date"]}')
+                    await asyncio.sleep(1)
 
             
