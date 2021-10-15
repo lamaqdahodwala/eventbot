450f87481c5f37fc80b1a3d81e453f458ceee58f
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-10-15 18:04:12 -0400

    added basic functionality

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import discord.ext.commands as commands
 import os
-from .components.main import Main
+from components.main import Main
 
 bot = commands.Bot(command_prefix='*')
 
@@ -8,4 +8,6 @@ bot = commands.Bot(command_prefix='*')
 bot.add_cog(Main(bot))
 
 
-bot.run(os.environ['TOKEN'])
+# bot.run(os.environ['TOKEN'])
+# bot.run(os.environ['TOKEN'])
+bot.run('ODk4Njg5OTE1NjM2ODI2MTkz.YWn4NA._cpZyuJzlCsXeTIhJ6OeEc0nxzg')
--- components/main.py
+++ components/main.py
@@ -1,5 +1,17 @@
 import discord.ext.commands as commands
+import aiohttp
 
 class Main(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot = bot
+        self.session = aiohttp.ClientSession
+
+    @commands.command(name='events')
+    async def show_events(self, ctx):
+        async with self.session() as session:
+            resp = await session.get('https://thrillshare-cmsv2.services.thrillshare.com/api/v4/o/5080/cms/events?locale=en')
+            json = await resp.json()
+            event = json['events'][0]
+            await ctx.send(f'{event["title"]} - {event["cms_formatted_date"]}')
+
+            
