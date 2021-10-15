f56956e4f3a7148372cbdbddd8f3eb9a586c6b5c
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-10-15 17:44:16 -0400

    bot can run now

--- bot.py
+++ bot.py
@@ -1,7 +1,11 @@
 import discord.ext.commands as commands
 import os
-
+from .components.main import Main
 
 bot = commands.Bot(command_prefix='*')
+
+
+bot.add_cog(Main(bot))
+
 
 bot.run(os.environ['TOKEN'])
--- components/main.py
+++ components/main.py
@@ -1,4 +1,5 @@
 import discord.ext.commands as commands
 
 class Main(commands.Cog):
-    ...
+    def __init__(self, bot) -> None:
+        self.bot = bot
