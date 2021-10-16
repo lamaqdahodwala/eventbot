bc4fa39675e9beec7e1b38467d83754e4598258e
Lamaq Dahodwala <74932012+lamaqdahodwala@users.noreply.github.com>
2021-10-16 23:45:14 +0000

    error handling

--- components/teachers.py
+++ components/teachers.py
@@ -8,11 +8,11 @@ class Teachers(commands.Cog):
         self.session = aiohttp.ClientSession
 
     @commands.command()
-    async def search(self, ctx, teacher):
+    async def search(self, ctx, teacher=None):
         embed = discord.Embed(title='Results')
         embed.color = discord.Color.green()
         async with self.session() as session:
-            resp = await session.get(f'https://thrillshare-cmsv2.services.thrillshare.com/api/v4/o/5080/cms/directories?locale=en&search={teacher}&filter_ids=')           
+            resp = await session.get(f'https://thrillshare-cmsv2.services.thrillshare.com/api/v4/o/5080/cms/directories?locale=en&search={teacher}&filter_ids=')       
             json = await resp.json()
             dirs = json['directories']
             for i in dirs:
